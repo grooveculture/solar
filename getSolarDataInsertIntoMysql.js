e819c48f9c2d6253fb051292169b44090e53b480
grooveculture <danielschaerer@protonmail.com>
2021-01-08 23:52:31 +0100

    adjust

--- getSolarDataInsertIntoMysql.js
+++ getSolarDataInsertIntoMysql.js
@@ -65,9 +65,9 @@ function insertData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const { Sequelize } = require('sequelize');
-      const sequelize = new Sequelize('solar', 'root', 'jtQNnPShsY2dbP', {
-        host: 'localhost',
-        dialect: 'mysql'
+      const sequelize = new Sequelize(process.env.DB_DB, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
       });
 
       sequelize.authenticate()
